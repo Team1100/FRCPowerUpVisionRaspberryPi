51630ae8ffce239f4c85985a6d64525aca3d8784
GrantPerkins <gcperk20@gmail.com>
2018-04-02 08:40:28 -0400

    Implement PyNetworktables client.py now writes the centerx and centery values to the SmartDashboard table. Todo: read the values.

--- client.py
+++ client.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 from cscore import CameraServer
 from grip import GripPipeline
-
+from networktables import NetworkTables
+from networktables.util import ntproperty
+import logging
+logging.basicConfig(level=logging.DEBUG)
 """
 Run this file to process vision code.
 
@@ -12,12 +15,11 @@ By Grant Perkins, 2018
 """
 def main():
     pipe = GripPipeline()
+    NetworkTables.initialize(server="roborio-1100-frc.local")
+    table = NetworkTables.getTable('SmartDashboard')
     img = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
-
     cs = CameraServer.getInstance()
-
     camera = cs.startAutomaticCapture()
-
     camera.setResolution(640, 480)
 
     # Get a CvSink. This will capture images from the camera
@@ -28,6 +30,8 @@ def main():
         # in the source image.  If there is an error notify the output.
         time, img = cvsink.grabFrame(img)
         cx, cy = pipe.process(img)
+        table.putNumber("centerx", cx)
+        table.putNumber("centery", cy)
 
     #print(pipe.process(cv2.imread("cubecorner.jpg",1))) #to test pipeline
 
